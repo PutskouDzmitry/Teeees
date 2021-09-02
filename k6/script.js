cfbd49e0133f635d233837db7914c1dfb0d15b7e
PutskouDzmitry <70838579+PutskouDzmitry@users.noreply.github.com>
2021-09-02 17:49:27 +0300

    Update script.js

--- k6/script.js
+++ k6/script.js
@@ -9,6 +9,5 @@ export default function () {
     http.get('http://localhost:8080/get?from=2019-02-01&to=2021-02-04&spaceId=Gomel_BrL2_turnstile');
     http.get(`http://localhost:8080/getCount?field=id`);
     http.put(`http://localhost:8080/update?field=id&value=1234&toId=10&fromId=10000`)
-    // http.delete(` http://localhost:8080/delete?id=121321`)
     sleep(1);
 }
